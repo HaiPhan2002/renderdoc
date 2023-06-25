a5b1d0820a0f27f1498c80d59ef840e25fc6c396
HaiPhan2002 <phannhuthai@gmail.com>
2023-06-25 14:33:32 +0700

    Add clean param and build x86_64

--- build-android.bat
+++ build-android.bat
@@ -1,3 +1,5 @@
 @echo off
-%ANDROID_HOME%\cmake\3.22.1\bin\cmake -DBUILD_ANDROID=On -DANDROID_ABI=armeabi-v7a .\CMakeLists.txt -DCMAKE_MAKE_PROGRAM=%ANDROID_HOME%\cmake\3.22.1\bin\ninja.exe --log-level=VERBOSE -GNinja
+if "%1" == "clean" %ANDROID_HOME%\cmake\3.22.1\bin\cmake --build . --target clean
+@REM %ANDROID_HOME%\cmake\3.22.1\bin\cmake -DBUILD_ANDROID=On -DANDROID_ABI=armeabi-v7a .\CMakeLists.txt -DCMAKE_MAKE_PROGRAM=%ANDROID_HOME%\cmake\3.22.1\bin\ninja.exe --log-level=VERBOSE -GNinja
+%ANDROID_HOME%\cmake\3.22.1\bin\cmake -DBUILD_ANDROID=On -DANDROID_ABI=x86_64 .\CMakeLists.txt -DCMAKE_MAKE_PROGRAM=%ANDROID_HOME%\cmake\3.22.1\bin\ninja.exe --log-level=VERBOSE -GNinja
 %ANDROID_HOME%\cmake\3.22.1\bin\ninja.exe
