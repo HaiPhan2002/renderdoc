f3d37c9fd75f1a96bd3b81ae2b1b7c9db0f08637
HaiPhan2002 <phannhuthai@gmail.com>
2023-06-25 19:48:40 +0700

    rename build_android.bat

--- build_android.bat
+++ build_android.bat
@@ -1,5 +1,5 @@
 @echo off
 if "%1" == "clean" %ANDROID_HOME%\cmake\3.22.1\bin\cmake --build . --target clean
 @REM %ANDROID_HOME%\cmake\3.22.1\bin\cmake -DBUILD_ANDROID=On -DANDROID_ABI=armeabi-v7a .\CMakeLists.txt -DCMAKE_MAKE_PROGRAM=%ANDROID_HOME%\cmake\3.22.1\bin\ninja.exe --log-level=VERBOSE -GNinja
-%ANDROID_HOME%\cmake\3.22.1\bin\cmake -DBUILD_ANDROID=On -DANDROID_ABI=x86_64 .\CMakeLists.txt -DCMAKE_MAKE_PROGRAM=%ANDROID_HOME%\cmake\3.22.1\bin\ninja.exe --log-level=VERBOSE -GNinja
+%ANDROID_HOME%\cmake\3.22.1\bin\cmake -DBUILD_ANDROID=On -DANDROID_ABI=x86_64 -DAPK_TARGET_ID=android-33 .\CMakeLists.txt -DCMAKE_MAKE_PROGRAM=%ANDROID_HOME%\cmake\3.22.1\bin\ninja.exe --log-level=VERBOSE -GNinja
 %ANDROID_HOME%\cmake\3.22.1\bin\ninja.exe
